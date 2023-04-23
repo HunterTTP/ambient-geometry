353fbb040e868748456bd53435c5850930c78c19
HunterTTP <HunterTTP@gmail.com>
2023-04-23 10:15:13 -0400

    Updated main.js and orbitcontrols.js to reference libs properly

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,5 @@
-import * as THREE from '/static/libs/three.module.js';
-import { OrbitControls } from '/static/libs/OrbitControls.js';
+import * as THREE from '../libs/three.module.js';
+import { OrbitControls } from '../libs/OrbitControls.js';
 
 let camera, scene, renderer;
 let plane;
